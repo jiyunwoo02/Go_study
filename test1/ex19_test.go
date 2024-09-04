3a5ec8105ddc01659f42af9b1737012999b33def
jiyunwoo <jiyunwoo33@gmail.com>
2024-09-04 17:12:36 +0900

    0904 Go 최종스터디

--- test1/ex19_test.go
+++ test1/ex19_test.go
@@ -17,4 +17,19 @@ func TestSquare3(t *testing.T) {
 func TestSquare4(t *testing.T) {
 	assert := assert.New(t)
 	assert.Equal(9, Square(3), "Square(3) should be 9")
+	// assert.Equal(4, Square(3), "Square(3) should be 9")
 }
+
+/*
+--- FAIL: TestSquare4 (0.00s)
+    ex19_test.go:19:
+                Error Trace:    C:/Users/jiyun/OneDrive/바탕 화면/goproject/test1/ex19_test.go:19
+                Error:          Not equal:
+                                expected: 4
+                                actual  : 9
+                Test:           TestSquare4
+                Messages:       Square(3) should be 9
+FAIL
+exit status 1
+FAIL    goproject/test1 0.821s
+*/
